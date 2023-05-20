51dd4572beb2ed9a134aac30e871703267c3704e
Muhammad Ian Kurniawan <ian00137@gmail.com>
2023-05-20 10:41:36 +0800

    2

--- public/script.js
+++
@@ -1,6 +0,0 @@
-const navButton = document.getElementById("nav-button");
-let navMobile = document.getElementById("nav-content");
-
-function navButtonClick(){
-    navMobile.classList.toggle("nav-trigger");
-}
